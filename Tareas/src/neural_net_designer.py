ad0671b0de0b15aa6fa71cb9b841357dea5ebf46
Ricardo <r.x.mg1801@gmail.com>
2026-01-20 02:52:20 -0600

    Refactor neural network designer: - Updated `parameters_estimator` to include a new parameter `K` for flexibility in parameter calculations. - Enhanced `NetworkDesigner` class and its subclasses to accommodate the new `K` parameter in design methods and dataset validation. - Removed legacy files `nn_desing.py` and `regression_desing.py` as their functionality is now integrated into the main designer. - Added a new `MulticlassClassificationDesigner` class for handling multiclass classification tasks. - Improved documentation and comments for clarity and maintainability.

--- Tareas/src/neural_net_designer.py
+++ Tareas/src/neural_net_designer.py
@@ -1,8 +1,3 @@
-# ==================================================================
-# Neural network designer for regressiona and binary classification
-# tasks based in heuristic.
-# ==================================================================
-
 import math
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
@@ -28,7 +23,7 @@ def clip(x: float, lo: float, hi: float) -> float:
     return max(lo, min(hi, x))
 
 
-def parameters_estimator(n0: int, layers: List[int]) -> int:
+def parameters_estimator(n0: int, layers: List[int], K:int) -> int:
     
     if not layers:
         return 0
@@ -36,7 +31,8 @@ def parameters_estimator(n0: int, layers: List[int]) -> int:
     P = (n0 + 1) * layers[0]
     for i in range(1, len(layers)):
         P += (layers[i - 1] + 1) * layers[i]
-    P += (layers[-1] + 1) * 1
+        
+    P += (layers[-1] + 1) * K
     return int(P)
 
 
@@ -45,18 +41,17 @@ def parameters_estimator(n0: int, layers: List[int]) -> int:
 ####################### 
 
 class NetworkDesigner(ABC): 
-    """
-    Template medthod to create deep layers
-    """
-    
-    def __init__(
-        self, 
-        k: int = 10, 
-        c1: float = 2.0,
-        r: float = 0.5,
-        n_min: int = 8,
-        L_max: int = 4):
-        
+   
+    def __init__(self,  k: int = 10,  c1: float = 2.0, r: float = 0.5, n_min: int = 8, L_max: int = 4):      
+        """ Architecture designer of neural network for different tasks based in heuristic. 
+
+        Args:
+            k (int): Max of parameters for data. By default 10.
+            c1 (float): Initial factor expansion. By default  2.0.
+            r (float): Reduction rate between layers. By default 0.5.
+            n_min (int): Min size of hidden layer . By default  8.
+            L_max (int): Max of hidden layers. By defaults 4.
+        """
         self.k = k
         self.c1 = c1 
         self.r  = r 
@@ -65,15 +60,15 @@ class NetworkDesigner(ABC):
         
     # ---------------- Template Method ---------------- # 
     
-    def desing(self, d:int, n0:int) -> NNDesing: 
+    def desing(self, d:int, n0:int, K: int) -> NNDesing: 
         
-        self._dataset_validation(d, n0)
+        self._dataset_validation(d, n0, K)
         
         n_max  = min(1024, max(64, math.floor(0.25*d)))
         P_max = math.floor(self.k*d)
         
         layers = self._layers_building(n0, n_max, P_max)
-        P = self._fitting_complexity(layers, n0, P_max)
+        P = self._fitting_complexity(layers, n0, K, P_max)
         
         rho =  P/P_max if P_max > 0 else 1.0 
         dropouts = self._dropouts_estimator(d, layers, rho)
@@ -115,9 +110,9 @@ class NetworkDesigner(ABC):
         
         return layers    
     
-    def _fitting_complexity(self, layers: List[int], n0:int, P_max:int) -> int: 
+    def _fitting_complexity(self, layers: List[int], n0:int, K: int, P_max:int) -> int: 
         
-        P = parameters_estimator(n0 = n0, layers = layers)
+        P = parameters_estimator(n0 = n0, layers = layers, K=K)
         
         while P> P_max: 
             if len(layers) > 1: 
@@ -130,7 +125,7 @@ class NetworkDesigner(ABC):
                 if layers[0] < self.n_min: 
                     raise ValueError("Not allowed layer >= n_min")
                 
-            P = parameters_estimator(n0 = n0, layers = layers)
+            P = parameters_estimator(n0 = n0, layers = layers, K=K)
         
         return P 
     
@@ -184,7 +179,7 @@ class NetworkDesigner(ABC):
     # ---------------- Strategy Methods ---------------- #
     
     @abstractmethod
-    def _dataset_validation(self, d:int, n0:int) -> None: 
+    def _dataset_validation(self, d:int, n0:int, K:int) -> None: 
         pass
     
     
@@ -194,21 +189,72 @@ class NetworkDesigner(ABC):
 
 class RegressionDesigner(NetworkDesigner):     
     
-   
-    def desing(self, d: int, n0: int) -> NNDesing:
-        return super().desing(d, n0)
+    def desing(self, d: int, n0: int, K:int = 1) -> NNDesing:
+        return super().desing(d, n0, K)
     
-    def _dataset_validation(self, d:int, n0: int) -> None: 
+    def _dataset_validation(self, d:int, n0: int, K:int = 1) -> None: 
         if d<2*n0: 
             raise ValueError("Small dataset for regression task")
+        
+        if K > 1: 
+            raise ValueError("For regression task, K must be equal to 1")
         
 
 class BinaryClassificationDesigner(NetworkDesigner):
      
-    def desing(self, d: int, n0: int) -> NNDesing:
-        return super().desing(d, n0)
+    def desing(self, d: int, n0: int, K:int =1) -> NNDesing:
+        return super().desing(d, n0, K)
    
-    def _dataset_validation(self, d: int, n0: int) -> None:
+    def _dataset_validation(self, d: int, n0: int, K:int =1) -> None:
         if d<2*n0: 
             raise ValueError("Small dataset for binary classification task")
+        if K > 1: 
+            raise ValueError("For binary classification task, K must be equal to 1")
         
+class MulticlassClassificationDesigner(NetworkDesigner): 
+    
+    def desing(self, d: int, n0: int, K: int) -> NNDesing:
+        return super().desing(d, n0, K)
+    
+    def _dataset_validation(self, d: int, n0: int, K:int) -> None:
+        if d<2*n0: 
+            raise ValueError("Small dataset for binary classification task")
+        if K < 3: 
+            raise ValueError("For multiclass classification task, K must be grater than 2")
+        
+        
+        
+        
+        
+if __name__ == "__main__":
+    """
+    this is a simple test
+    """
+    
+    d =  4436
+    n0 =  44
+    K1 = 1
+    K2 = 4
+    
+    regression_task = RegressionDesigner()
+    regression_desing = regression_task.desing(d = d, n0=n0, K = K1)
+    print('='*60)
+    print("   Regression desing   ")
+    print(regression_desing)
+    print('-'*60, '\n')
+    
+    
+    binary_task = BinaryClassificationDesigner()
+    binary_desing = binary_task.desing(d = d, n0=n0, K=K1)
+    print('='*60)
+    print("   Binary desing   ")
+    print(binary_desing)
+    print('-'*60, '\n')
+    
+    
+    multiclass_task = MulticlassClassificationDesigner()
+    multiclass_desing = multiclass_task.desing(d = d, n0=n0, K = K2)
+    print('-'*60)
+    print("   Multiclassification desing   ")
+    print(multiclass_desing)
+    print('-'*60, '\n')
